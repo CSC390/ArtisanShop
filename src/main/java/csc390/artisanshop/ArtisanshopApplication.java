0ebf09712133f586f684ef57b34ad59640acb638
‘Elena Voinu <lenka36@gmail.com>
2023-09-15 07:32:05 -0500

    Initial commit

---
+++ src/main/java/csc390/artisanshop/ArtisanshopApplication.java
@@ -0,0 +1,13 @@
+package csc390.artisanshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisanshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtisanshopApplication.class, args);
+	}
+
+}
